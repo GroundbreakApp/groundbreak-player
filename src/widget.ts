69bbfc88e669fbbd23c7a840782d080c84f887df
baymax <pony.kadeem@gmail.com>
2023-05-25 18:40:07 -0400

    fix: remove wrong letter in template

--- src/widget.ts
+++ src/widget.ts
@@ -55,7 +55,7 @@ template.innerHTML = `
   </style>
 
   <div id="groundbreak-widget-container" style="position: absolute; margin: auto; left:0; right:0; width: 100%; height: 100%; z-index: 99999; pointer-events: none">
-  </div>;
+  </div>
 `;
 
 class GroundWidget extends globalThis.HTMLElement {
